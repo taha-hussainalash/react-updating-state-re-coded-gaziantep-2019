2fda6cd09276836cbd81a50980f2e84ed16a42c1
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-11 14:48:13 +0000

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,45 @@
 // Code ClickityClick Component Here
+import React from 'react';
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hasBeenClicked: true
+    };
+  }
+
+  handleClick = () => {
+  this.setState({
+    const house = {
+  kitchen: {
+    cabinets: 'white',
+    table: {
+      legs: 4
+    }
+  }
+}
+ 
+const updatedHouse = deepMerge(house, {
+  kitchen: {
+    table: {
+      legs: 8
+    }
+  }
+})
+    
+    hasBeenClicked: true
+ () => console.log(this.state.hasBeenClicked))
+  
+}
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+  }
+
+
+export default ClickityClick;
